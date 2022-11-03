3ab1dec417db6b834f90eeb94ae453a056bc147a
norm <stream@onyxandiris.online>
2022-11-03 01:38:55 +0000

    rename exported factory functions.

--- lib/OBSWebSocket.psm1
+++ lib/OBSWebSocket.psm1
@@ -2,14 +2,14 @@
 . $PSScriptRoot\events.ps1
 
 
-Function Get-Request {
+Function Get-OBSRequest {
     param($hostname, $port = 4455, $pass)
     return [Request]::new($hostname, $port, $pass)
 }
 
-Function Get-Event {
+Function Get-OBSEvent {
     param($hostname, $port = 4455, $pass, $subs = $(Get-LowVolume))
     return [Event]::new($hostname, $port, $pass, $subs)
 }
 
-Export-ModuleMember -Function Get-Request, Get-Event, Get-Subs, Get-LowVolume, Get-HighVolume, Get-All
+Export-ModuleMember -Function Get-OBSRequest, Get-OBSEvent, Get-Subs, Get-LowVolume, Get-HighVolume, Get-All
